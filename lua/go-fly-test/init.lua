d8597aec73444ba632d6779ad2be137fddce138c
Pablo Triandafilide <pablotrianda@gmail.com>
2025-12-13 22:14:59 -0300

    Remove go-fly-test and use a native neovim list

--- lua/go-fly-test/init.lua
+++ lua/go-fly-test/init.lua
@@ -1,48 +1,55 @@
-local hydra = require "hydra"
-
-local hint = [[
-   _f_: Test File     _x_: Compile test
-   _c_: Current Test  _t_: Test Coverage report (diff) 
-   _o_: Open coverage report 
- ^
- _<Esc>_
-]]
-
-local function create_hydra()
-   hydra({
-      name = "GoFlyTest",
-      hint = hint,
-      config = {
-         color = "teal",
-         invoke_on_body = true,
-         hint = {
-            position = "middle",
-            border = "rounded",
-         },
-      },
-      mode = "n",
-      body = "<leader>t",
-      heads = {
-         { "f", ":GoTestFile<CR>" },
-         { "c", ":GoTestFunc<CR>" },
-         { "x", ":GoTestCompile<CR>" },
-         { "t", ":GoCoverage<CR>" },
-         {
-            "o",
-            ":!go test ./... -coverprofile=cover.out && go tool cover -html=cover.out && go tool cover -func=cover.out<CR>"
-         },
-         { "<Esc>", nil, { exit = true, nowait = true } },
-      },
-   })
-end
-
 local M = {}
 
-M.setup = function()
-   vim.api.nvim_create_autocmd("FileType", {
-      pattern = "go",
-      callback = create_hydra,
-   })
+local actions = {
+  {
+    label = "Test File",
+    cmd = "GoTestFile",
+  },
+  {
+    label = "Current Test",
+    cmd = "GoTestFunc",
+  },
+  {
+    label = "Compile Test",
+    cmd = "GoTestCompile",
+  },
+  {
+    label = "Coverage report",
+    cmd = "GoCoverage",
+  },
+}
+
+local function run_action(action)
+  vim.api.nvim_cmd({ cmd = action.cmd }, {})
+end
+
+function M.open_menu()
+  vim.ui.select(actions, {
+    prompt = "Go Test",
+    format_item = function(item)
+      return item.label
+    end,
+  }, function(choice)
+    if choice then
+      run_action(choice)
+    end
+  end)
+end
+
+function M.setup()
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "go",
+    callback = function()
+      vim.keymap.set(
+        "n",
+        "<leader>t",
+        M.open_menu,
+        { buffer = true, desc = "Go Test Menu" }
+      )
+    end,
+  })
 end
 
 return M
+
+
